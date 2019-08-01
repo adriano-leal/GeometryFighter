cb667caa0b0ce95f357df64a6e187e031b838a64
Adriano Leal Ramos <ramos.adrianoleal@gmail.com>
2019-08-01 11:00:31 -0300

    part 5 done

--- sceneKitGame_challenge/GameViewController.swift
+++ sceneKitGame_challenge/GameViewController.swift
@@ -16,6 +16,7 @@ class GameViewController: UIViewController {
     var scnScene: SCNScene!
     var cameraNode: SCNNode!
     var spawnTime: TimeInterval = 0
+    var game = GameHelper.sharedInstance
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +25,7 @@ class GameViewController: UIViewController {
         setupScene()
         setupCamera()
         spawnShape()
+        setupHUD()
     }
     
     override var shouldAutorotate: Bool {
@@ -38,7 +40,7 @@ class GameViewController: UIViewController {
         scnView = self.view as! SCNView
         
         scnView.showsStatistics = true
-        scnView.allowsCameraControl = true
+        scnView.allowsCameraControl = false // true
         scnView.autoenablesDefaultLighting = true
         scnView.delegate = self
         scnView.isPlaying = true
@@ -80,7 +82,8 @@ class GameViewController: UIViewController {
             geometry = SCNTube(innerRadius: 0.25, outerRadius: 0.5, height: 1.0)
         }
         
-        geometry.materials.first?.diffuse.contents = UIColor.random()
+        let color = UIColor.random()
+        geometry.materials.first?.diffuse.contents = color
         
         let geometryNode = SCNNode(geometry: geometry)
         geometryNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
@@ -92,6 +95,15 @@ class GameViewController: UIViewController {
         
         geometryNode.physicsBody?.applyForce(force, at: position, asImpulse: true)
         
+        let trailEmitter = createTrail(color: color, geometry: geometry)
+        geometryNode.addParticleSystem(trailEmitter)
+        
+        if color == UIColor.black {
+            geometryNode.name = "BAD"
+        } else {
+            geometryNode.name = "GOOD"
+        }
+        
         scnScene.rootNode.addChildNode(geometryNode)
     }
     
@@ -101,6 +113,55 @@ class GameViewController: UIViewController {
                 node.removeFromParentNode()
             }
         }
+    }
+    
+    func createTrail(color: UIColor, geometry: SCNGeometry) -> SCNParticleSystem {
+        let trail = SCNParticleSystem(named: "Trail.scnp", inDirectory: nil)!
+        trail.particleColor = color
+        trail.emitterShape = geometry
+    
+        return trail
+    }
+    
+    func setupHUD() {
+        game.hudNode.position = SCNVector3(0.0, 10.0, 0.0)
+        scnScene.rootNode.addChildNode(game.hudNode)
+    }
+    
+    func handleTouchFor(node: SCNNode) {
+        if node.name == "GOOD" {
+            game.score += 1
+            createExplosion(geometry: node.geometry!, position: node.presentation.position, rotation: node.presentation.rotation)
+            node.removeFromParentNode()
+        } else if node.name == "BAD" {
+            game.lives -= 1
+            createExplosion(geometry: node.geometry!, position: node.presentation.position, rotation: node.presentation.rotation)
+            node.removeFromParentNode()
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let touch = touches.first!
+        let location = touch.location(in: scnView)
+        let hitResults = scnView.hitTest(location, options: nil)
+        
+        if let result = hitResults.first {
+            handleTouchFor(node: result.node)
+        }
+    }
+    
+    func createExplosion(geometry: SCNGeometry, position: SCNVector3, rotation: SCNVector4) {
+        
+        let explosion = SCNParticleSystem(named: "Explode.scnp", inDirectory: nil)!
+        explosion.emitterShape = geometry
+        explosion.birthLocation = .surface
+        
+        let rotationMatrix = SCNMatrix4MakeRotation(rotation.w, rotation.x, rotation.y, rotation.z)
+        let translationMatrix = SCNMatrix4MakeTranslation(position.x, position.y, position.z)
+        let transformMatrix = SCNMatrix4Mult(rotationMatrix, translationMatrix)
+        
+        scnScene.addParticleSystem(explosion, transform: transformMatrix)
     }
 }
 
@@ -114,5 +175,6 @@ extension GameViewController: SCNSceneRendererDelegate {
         }
         
         cleanScene()
+        game.updateHUD()
     }
 }
